34de805076aa9ddec4eb6e962bd01e0636a926c0
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 09:13:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,8 @@ def game_hash
     home => {
       :team_name => "Brooklyn Nets",
       :colors => "Black, White",
-      :players => {
+      :players => [
+        {
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
@@ -15,9 +16,31 @@ def game_hash
         :assists => 12,
         :steals => 3,
         :blocks => 1,
-        :slam_dunks => 1,
-      }
-
+        :slam_dunks => 1
+      },
+      {
+      :player_name => "Reggie Evans",
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
+    },
+    {
+      :player_name => "Reggie Evans",
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
+    },
+    ]
     }
   }
 
