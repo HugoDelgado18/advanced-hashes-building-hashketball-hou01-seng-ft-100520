4da894bb5b845d134580421351b4bfed34243250
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 08:35:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def game_hash(hash)
 
-  hash.each_with_object({}) do |(key, value), new_hash|
+  hash.each_with_object({}) do |(key, value), game_hash|
     value.each do |inner_keys, inner_value|
       binding.pry
     end
