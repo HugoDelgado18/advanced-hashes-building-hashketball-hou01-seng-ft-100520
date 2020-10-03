a5123eea3f7a6377729dc0802d5b90cfbc3861df
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 08:23:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,12 +4,14 @@ def game_hash(hash)
 
   final_result = hash.each_with_object({}) do |(key, value), new_hash|
     values.each do |inner_keys, inner-value|
+      binding.pry
     end
+    binding.pry
   end
 
 
 
-
+binding.pry
 
 
 
